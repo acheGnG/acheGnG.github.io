772bf20a2b1dededfaebf09ceb464c2ddaa84026
Chan Jun Wei <chanjunweimy@gmail.com>
2017-01-22 08:24:35 +0800

    add constants
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- js/index.js
+++ js/index.js
@@ -6,7 +6,41 @@ const WHAT_COLOR = "What color is it?";
 const WHAT_SIZE = "What size is it?";
 const IS_HEADACHE_GONE = "Is your headache gone now? By now it should be gone :) If not, click continue.";
 
+const CHI_WHERE_HEADACHE = "你的头痛在哪里？";
+const CHI_WHERE_HEADACHE_NOW = "你的头痛现在在哪里？";
+const CHI_WHAT_SHAPE = "它是什么形状？";
+const CHI_WHAT_COLOR = "它是什么颜色?";
+const CHI_WHAT_SIZE = "它是什么尺寸？";
+const CHI_IS_HEADACHE_GONE = "你还有头痛吗？现在应该不觉得头痛了吧？ :) 如果还觉得头痛，请点击 continue。";
 
+const JAP_WHERE_HEADACHE = "你的头痛在哪里？";
+const JAP_WHERE_HEADACHE_NOW = "你的头痛现在在哪里？";
+const JAP_WHAT_SHAPE = "它是什么形状？";
+const JAP_WHAT_COLOR = "它是什么颜色？";
+const JAP_WHAT_SIZE = "它是什么尺寸？";
+const JAP_IS_HEADACHE_GONE = "你还有头痛吗？现在应该不觉得头痛了吧？ :) 如果还觉得头痛，请点击 continue。";
+
+const FR_WHERE_HEADACHE = "Où est votre mal à la tête?";
+const FR_WHERE_HEADACHE_NOW = "Où est votre mal à la tête maintenant?";
+const FR_WHAT_SHAPE = "Quelle est-ce forme?";
+const FR_WHAT_COLOR = "Quelle est-ce couleur?";
+const FR_WHAT_SIZE = "quelle est-ce taille?";
+const FR_IS_HEADACHE_GONE = "Est-ce que vous avez mal à la tête? Vous ne devrait pas avoir mal à la tête maintenant :) " + 
+                            "Si vous avez mal à la tête encore, veuillez appuyer sur continue";
+
+const CAN_WHERE_HEADACHE = "你的头痛在哪里？";
+const CAN_WHERE_HEADACHE_NOW = "你的头痛现在在哪里？";
+const CAN_WHAT_SHAPE = "它是什么形状？";
+const CAN_WHAT_COLOR = "它是什么颜色？";
+const CAN_WHAT_SIZE = "它是什么尺寸？";
+const CAN_IS_HEADACHE_GONE = "你还有头痛吗？现在应该不觉得头痛了吧？ :) 如果还觉得头痛，请点击 continue。";
+
+const MAL_WHERE_HEADACHE = "Mana sakit kepala mu?";
+const MAL_WHERE_HEADACHE_NOW = "Mana sakit kepala mu sekarang?";
+const MAL_WHAT_SHAPE = "Apa bentuk itu?";
+const MAL_WHAT_COLOR = "Apa warna itu?";
+const MAL_WHAT_SIZE = "Apa saiz itu?";
+const MAL_IS_HEADACHE_GONE = "Kamu masih sakit kepala? Sekarang sepatutnya tak berasa sakit kepala lagi :) Kalau kamu masih berasa sakit kepala, sila tekan continue。";
 
 const WHERE_HEADACHE_ID = "a1";
 const WHERE_HEADACHE_NOW_ID = "a2";
@@ -15,12 +49,40 @@ const WHAT_COLOR_ID = "a4";
 const WHAT_SIZE_ID = "a5";
 const IS_HEADACHE_GONE_ID = "a6";
 
-const WHERE_HEADACHE_LINK = "audio/where_is_your_headache.mp3";
-const WHERE_HEADACHE_NOW_LINK = "audio/where_is_your_headache_now.mp3";
-const WHAT_SHAPE_LINK = "audio/what_shape_issit.mp3";
-const WHAT_COLOR_LINK = "audio/what_color_issit.mp3";
-const WHAT_SIZE_LINK = "audio/what_size_issit.mp3";
-const IS_HEADACHE_GONE_LINK = "audio/headache_gone_now.mp3";
+const CHI_WHERE_HEADACHE_ID = "a1";
+const CHI_WHERE_HEADACHE_NOW_ID = "a2";
+const CHI_WHAT_SHAPE_ID = "a3";
+const CHI_WHAT_COLOR_ID = "a4";
+const CHI_WHAT_SIZE_ID = "a5";
+const CHI_IS_HEADACHE_GONE_ID = "a6";
+
+const JAP_WHERE_HEADACHE_ID = "a1";
+const JAP_WHERE_HEADACHE_NOW_ID = "a2";
+const JAP_WHAT_SHAPE_ID = "a3";
+const JAP_WHAT_COLOR_ID = "a4";
+const JAP_WHAT_SIZE_ID = "a5";
+const JAP_IS_HEADACHE_GONE_ID = "a6";
+
+const FR_WHERE_HEADACHE_ID = "a1";
+const FR_WHERE_HEADACHE_NOW_ID = "a2";
+const FR_WHAT_SHAPE_ID = "a3";
+const FR_WHAT_COLOR_ID = "a4";
+const FR_WHAT_SIZE_ID = "a5";
+const FR_IS_HEADACHE_GONE_ID = "a6";
+
+const CAN_WHERE_HEADACHE_ID = "a1";
+const CAN_WHERE_HEADACHE_NOW_ID = "a2";
+const CAN_WHAT_SHAPE_ID = "a3";
+const CAN_WHAT_COLOR_ID = "a4";
+const CAN_WHAT_SIZE_ID = "a5";
+const CAN_IS_HEADACHE_GONE_ID = "a6";
+
+const MAL_WHERE_HEADACHE_ID = "a1";
+const MAL_WHERE_HEADACHE_NOW_ID = "a2";
+const MAL_WHAT_SHAPE_ID = "a3";
+const MAL_WHAT_COLOR_ID = "a4";
+const MAL_WHAT_SIZE_ID = "a5";
+const MAL_IS_HEADACHE_GONE_ID = "a6";
 
 const LANGUAGE_ENGLISH = "English";
 const LANGUAGE_CHINESE = "Chinese";
@@ -46,44 +108,44 @@ engAudioMap.set(WHAT_SIZE, WHAT_SIZE_ID);
 engAudioMap.set(IS_HEADACHE_GONE, IS_HEADACHE_GONE_ID);
 
 var chiAudioMap = new Map();
-chiAudioMap.set(WHERE_HEADACHE, WHERE_HEADACHE_ID);
-chiAudioMap.set(WHERE_HEADACHE_NOW, WHERE_HEADACHE_NOW_ID);
-chiAudioMap.set(WHAT_SHAPE, WHAT_SHAPE_ID);
-chiAudioMap.set(WHAT_COLOR, WHAT_COLOR_ID);
-chiAudioMap.set(WHAT_SIZE, WHAT_SIZE_ID);
-chiAudioMap.set(IS_HEADACHE_GONE, IS_HEADACHE_GONE_ID);
+chiAudioMap.set(CHI_WHERE_HEADACHE, CHI_WHERE_HEADACHE_ID);
+chiAudioMap.set(CHI_WHERE_HEADACHE_NOW, CHI_WHERE_HEADACHE_NOW_ID);
+chiAudioMap.set(CHI_WHAT_SHAPE, CHI_WHAT_SHAPE_ID);
+chiAudioMap.set(CHI_WHAT_COLOR, CHI_WHAT_COLOR_ID);
+chiAudioMap.set(CHI_WHAT_SIZE, CHI_WHAT_SIZE_ID);
+chiAudioMap.set(CHI_IS_HEADACHE_GONE, CHI_IS_HEADACHE_GONE_ID);
 
 var japAudioMap = new Map();
-japAudioMap.set(WHERE_HEADACHE, WHERE_HEADACHE_ID);
-japAudioMap.set(WHERE_HEADACHE_NOW, WHERE_HEADACHE_NOW_ID);
-japAudioMap.set(WHAT_SHAPE, WHAT_SHAPE_ID);
-japAudioMap.set(WHAT_COLOR, WHAT_COLOR_ID);
-japAudioMap.set(WHAT_SIZE, WHAT_SIZE_ID);
-japAudioMap.set(IS_HEADACHE_GONE, IS_HEADACHE_GONE_ID);
+japAudioMap.set(JAP_WHERE_HEADACHE, JAP_WHERE_HEADACHE_ID);
+japAudioMap.set(JAP_WHERE_HEADACHE_NOW, JAP_WHERE_HEADACHE_NOW_ID);
+japAudioMap.set(JAP_WHAT_SHAPE, JAP_WHAT_SHAPE_ID);
+japAudioMap.set(JAP_WHAT_COLOR, JAP_WHAT_COLOR_ID);
+japAudioMap.set(JAP_WHAT_SIZE, JAP_WHAT_SIZE_ID);
+japAudioMap.set(JAP_IS_HEADACHE_GONE, JAP_IS_HEADACHE_GONE_ID);
 
 var frAudioMap = new Map();
-frAudioMap.set(WHERE_HEADACHE, WHERE_HEADACHE_ID);
-frAudioMap.set(WHERE_HEADACHE_NOW, WHERE_HEADACHE_NOW_ID);
-frAudioMap.set(WHAT_SHAPE, WHAT_SHAPE_ID);
-frAudioMap.set(WHAT_COLOR, WHAT_COLOR_ID);
-frAudioMap.set(WHAT_SIZE, WHAT_SIZE_ID);
-frAudioMap.set(IS_HEADACHE_GONE, IS_HEADACHE_GONE_ID);
+frAudioMap.set(FR_WHERE_HEADACHE, FR_WHERE_HEADACHE_ID);
+frAudioMap.set(FR_WHERE_HEADACHE_NOW, FR_WHERE_HEADACHE_NOW_ID);
+frAudioMap.set(FR_WHAT_SHAPE, FR_WHAT_SHAPE_ID);
+frAudioMap.set(FR_WHAT_COLOR, FR_WHAT_COLOR_ID);
+frAudioMap.set(FR_WHAT_SIZE, FR_WHAT_SIZE_ID);
+frAudioMap.set(FR_IS_HEADACHE_GONE, FR_IS_HEADACHE_GONE_ID);
 
 var canAudioMap = new Map();
-canAudioMap.set(WHERE_HEADACHE, WHERE_HEADACHE_ID);
-canAudioMap.set(WHERE_HEADACHE_NOW, WHERE_HEADACHE_NOW_ID);
-canAudioMap.set(WHAT_SHAPE, WHAT_SHAPE_ID);
-canAudioMap.set(WHAT_COLOR, WHAT_COLOR_ID);
-canAudioMap.set(WHAT_SIZE, WHAT_SIZE_ID);
-canAudioMap.set(IS_HEADACHE_GONE, IS_HEADACHE_GONE_ID);
+canAudioMap.set(CAN_WHERE_HEADACHE, CAN_WHERE_HEADACHE_ID);
+canAudioMap.set(CAN_WHERE_HEADACHE_NOW, CAN_WHERE_HEADACHE_NOW_ID);
+canAudioMap.set(CAN_WHAT_SHAPE, CAN_WHAT_SHAPE_ID);
+canAudioMap.set(CAN_WHAT_COLOR, CAN_WHAT_COLOR_ID);
+canAudioMap.set(CAN_WHAT_SIZE, CAN_WHAT_SIZE_ID);
+canAudioMap.set(CAN_IS_HEADACHE_GONE, CAN_IS_HEADACHE_GONE_ID);
 
 var malAudioMap = new Map();
-malAudioMap.set(WHERE_HEADACHE, WHERE_HEADACHE_ID);
-malAudioMap.set(WHERE_HEADACHE_NOW, WHERE_HEADACHE_NOW_ID);
-malAudioMap.set(WHAT_SHAPE, WHAT_SHAPE_ID);
-malAudioMap.set(WHAT_COLOR, WHAT_COLOR_ID);
-malAudioMap.set(WHAT_SIZE, WHAT_SIZE_ID);
-malAudioMap.set(IS_HEADACHE_GONE, IS_HEADACHE_GONE_ID);
+malAudioMap.set(MAL_WHERE_HEADACHE, MAL_WHERE_HEADACHE_ID);
+malAudioMap.set(MAL_WHERE_HEADACHE_NOW, MAL_WHERE_HEADACHE_NOW_ID);
+malAudioMap.set(MAL_WHAT_SHAPE, MAL_WHAT_SHAPE_ID);
+malAudioMap.set(MAL_WHAT_COLOR, MAL_WHAT_COLOR_ID);
+malAudioMap.set(MAL_WHAT_SIZE, MAL_WHAT_SIZE_ID);
+malAudioMap.set(MAL_IS_HEADACHE_GONE, MAL_IS_HEADACHE_GONE_ID);
 
 var audioMaps = Array(engAudioMap,
                       chiAudioMap,
@@ -97,20 +159,20 @@ var langIndex = 0;
 var engFirstQuestionDatabase = Array(WHERE_HEADACHE,
                                      WHERE_HEADACHE_NOW);
 
-var chiFirstQuestionDatabase = Array(WHERE_HEADACHE,
-                                     WHERE_HEADACHE_NOW);
+var chiFirstQuestionDatabase = Array(CHI_WHERE_HEADACHE,
+                                     CHI_WHERE_HEADACHE_NOW);
 
-var japFirstQuestionDatabase = Array(WHERE_HEADACHE,
-                                     WHERE_HEADACHE_NOW);
+var japFirstQuestionDatabase = Array(JAP_WHERE_HEADACHE,
+                                     JAP_WHERE_HEADACHE_NOW);
 
-var frFirstQuestionDatabase = Array(WHERE_HEADACHE,
-                                     WHERE_HEADACHE_NOW);
+var frFirstQuestionDatabase = Array(FR_WHERE_HEADACHE,
+                                     FR_WHERE_HEADACHE_NOW);
 
-var canFirstQuestionDatabase = Array(WHERE_HEADACHE,
-                                     WHERE_HEADACHE_NOW);
+var canFirstQuestionDatabase = Array(CAN_WHERE_HEADACHE,
+                                     CAN_WHERE_HEADACHE_NOW);
 
-var malFirstQuestionDatabase = Array(WHERE_HEADACHE,
-                                     WHERE_HEADACHE_NOW);
+var malFirstQuestionDatabase = Array(MAL_WHERE_HEADACHE,
+                                     MAL_WHERE_HEADACHE_NOW);
 var firstQuestionDatabases = Array(engFirstQuestionDatabase,
                                   chiFirstQuestionDatabase,
                                   japFirstQuestionDatabase,
@@ -124,25 +186,25 @@ var engQuestionDatabase = Array(WHAT_COLOR,
                                 WHAT_SHAPE,
                                 WHAT_SIZE);
 
-var chiQuestionDatabase = Array(WHAT_COLOR,
-                                WHAT_SHAPE,
-                                WHAT_SIZE);
+var chiQuestionDatabase = Array(CHI_WHAT_COLOR,
+                                CHI_WHAT_SHAPE,
+                                CHI_WHAT_SIZE);
 
-var japQuestionDatabase = Array(WHAT_COLOR,
-                                WHAT_SHAPE,
-                                WHAT_SIZE);
+var japQuestionDatabase = Array(JAP_WHAT_COLOR,
+                                JAP_WHAT_SHAPE,
+                                JAP_WHAT_SIZE);
 
-var frQuestionDatabase = Array(WHAT_COLOR,
-                                WHAT_SHAPE,
-                                WHAT_SIZE);
+var frQuestionDatabase = Array(FR_WHAT_COLOR,
+                                FR_WHAT_SHAPE,
+                                FR_WHAT_SIZE);
 
-var canQuestionDatabase = Array(WHAT_COLOR,
-                                WHAT_SHAPE,
-                                WHAT_SIZE);
+var canQuestionDatabase = Array(CAN_WHAT_COLOR,
+                                CAN_WHAT_SHAPE,
+                                CAN_WHAT_SIZE);
 
-var malQuestionDatabase = Array(WHAT_COLOR,
-                                WHAT_SHAPE,
-                                WHAT_SIZE);
+var malQuestionDatabase = Array(MAL_WHAT_COLOR,
+                                MAL_WHAT_SHAPE,
+                                MAL_WHAT_SIZE);
 var questionDatabases = Array(engQuestionDatabase,
                                   chiQuestionDatabase,
                                   japQuestionDatabase,
@@ -151,11 +213,11 @@ var questionDatabases = Array(engQuestionDatabase,
                                   malQuestionDatabase);
 
 var englastQuestion = IS_HEADACHE_GONE;
-var chilastQuestion = IS_HEADACHE_GONE;
-var japlastQuestion = IS_HEADACHE_GONE;
-var frlastQuestion = IS_HEADACHE_GONE;
-var canlastQuestion = IS_HEADACHE_GONE;
-var mallastQuestion = IS_HEADACHE_GONE;
+var chilastQuestion = CHI_IS_HEADACHE_GONE_ID;
+var japlastQuestion = JAP_IS_HEADACHE_GONE;
+var frlastQuestion = FR_IS_HEADACHE_GONE;
+var canlastQuestion = CAN_IS_HEADACHE_GONE;
+var mallastQuestion = MAL_IS_HEADACHE_GONE;
 var lastQuestions = Array(englastQuestion,
                      chilastQuestion,
                      japlastQuestion,
@@ -187,14 +249,6 @@ function shuffle(array) {
 
     return array;
 }
-
-
-var firstQuestionDatabase = Array(WHERE_HEADACHE,
-                                   WHERE_HEADACHE_NOW);
-
-var questionDatabase = Array(WHAT_COLOR,
-                              WHAT_SHAPE,
-                              WHAT_SIZE);
 
 
 $('#lang').change(function () {
